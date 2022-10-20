6a713aa2c58612472d4985fbc383ddd56e95b4a3
Vaibhav Karale <vdk.karale@gmail.com>
2022-10-20 17:31:58 +0530

    login functionality adeded

--- login.js
+++ login.js
@@ -2,3 +2,13 @@
 let reg_email = abhishek@gmail.com
 
 let reg_pass = abhishek0888
+
+let email = abhishek@gmail.com
+
+let pass = abhishek0888
+
+if (reg_email == email && reg_pass == pass ){
+
+console.log("Login Succesfull !")
+
+}
